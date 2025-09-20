351c87781207d55a675403f1e2ecf8c931a1ea82
Teemu Viikeri <tepavi@gmail.com>
2025-09-20 18:04:43 +0300

    Add a function to get file contents from a given file

---
+++ functions/get_file_content.py
@@ -0,0 +1,22 @@
+import os
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        file_full_path = os.path.join(working_directory, file_path)
+        abs_work_dir = os.path.abspath(working_directory)
+        abs_file_full_path = os.path.abspath(file_full_path)
+
+        if not abs_file_full_path.startswith(abs_work_dir):
+            return f"Error: Cannot read \"{file_path}\" as it is outside the permitted working directory"
+
+        if not os.path.isfile(abs_file_full_path):
+            return f"Error: File not found or is not a regular file: \"{file_path}\""
+
+        with open(abs_file_full_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            return content + f"[...File \"{file_path}\" truncated at 10000 characters]"
+
+    except Exception as e:
+        return f"Error: {e}"
