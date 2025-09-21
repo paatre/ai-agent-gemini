703e3bc0af82d02450e767a79c9fde669d4edf93
Teemu Viikeri <tepavi@gmail.com>
2025-09-21 23:05:28 +0300

    Specify some overwriting and mathematical rules to system prompt

--- prompts.py
+++ prompts.py
@@ -9,4 +9,8 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+Don't overly overwrite files; only do so when necessary given the user's request. Especially, don't just add print statements to files unless explicitly asked to do so.
+
+If there's a mathematical change to be made to the calculator, you can run the mathemtical code in your Python interpreter to verify the results before making the change.We don't want our calculator to give different results than what a normal Python interpreter would give.
 """
